c8d25f685294c9062c3da61dc11a38e45287c85e
zaksiu12s <zaksiu279@gmail.com>
2025-01-12 15:54:23 +0100

    0.6.1 add light mode context provider

--- src/components/layout/SideNavBar/SideNavBarToggleTheme.tsx
+++ src/components/layout/SideNavBar/SideNavBarToggleTheme.tsx
@@ -1,16 +1,22 @@
 import SunIcon from "../../../assets/icons/Sun";
 import useCollapseSideNavBarContext from "../../../hooks/useCollapseSideNavBarContext";
+import useTheme from "../../../hooks/useTheme";
 
 export function SideNavBarToggleTheme() {
     const { isCollapsed } = useCollapseSideNavBarContext();
+    const { theme, setTheme } = useTheme();
+
+    const handleThemeChange = () => {
+        setTheme(theme === "dark" ? "light" : "dark")
+    }
 
     return (<div className="flex items-center px-2 text-gray-500 text-sm font-Roboto gap-2">
         <div className="w-5 h-5">
             <SunIcon />
         </div>
         {!isCollapsed && <p>Light Mode</p>}
-        <button type="button" title="Toggle Light/Dark Mode" className="relative bg-neutral-200 w-8 h-4 cursor-pointer ml-auto shadow-inner shadow-neutral-300 rounded-full border">
-            <div className="w-1/2 h-full absolute left-0 top-0 bg-white rounded-full shadow shadow-bg-neutral-300" />
+        <button type="button" title="Toggle Light/Dark Mode" className="relative bg-neutral-200 w-8 h-4 cursor-pointer ml-auto shadow-inner shadow-neutral-300 rounded-full border" onClick={handleThemeChange}>
+            <div className={`w-3.5 h-3.5 transition-all absolute ${theme == "light" ? "left-0" : "left-full -translate-x-full"} top-0 bg-white rounded-full shadow shadow-bg-neutral-300`} />
         </button>
     </div>);
 }
