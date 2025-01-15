3edaaee526248f3b866671ae2852359d9c5c44db
zaksiu12s <zaksiu279@gmail.com>
2025-01-15 17:22:01 +0100

    1.7.1 add functionality to ViewCard

--- src/components/layout/Dashboard/Overview/TotalViewsCard.tsx
+++ src/components/layout/Dashboard/Overview/TotalViewsCard.tsx
@@ -1,9 +1,19 @@
 import { ArrowIcon, EyeIcon } from "@icons";
 import { useTranslation } from "react-i18next";
 import CardContainer from "../../../features/CardContainer";
+import { useState } from "react";
+
+const calculateViewProgress = (currentMonth: number, lastMonth: number) => {
+    const progress = Math.round((currentMonth - lastMonth) / lastMonth * 100);
+
+    return progress;
+}
 
 export default function TotalViewsCard() {
     const { t } = useTranslation();
+    const [thisMonthViews] = useState<number>(1428);
+    const [lastMonthViews] = useState<number>(1024);
+    const [progress] = useState<number>(calculateViewProgress(thisMonthViews, lastMonthViews));
 
     return (
         <CardContainer>
@@ -14,12 +24,12 @@ export default function TotalViewsCard() {
                     </div>
                 </div>
                 <div className="text-sm font-bold font-Poppins pt-6 text-neutral-800 dark:text-neutral-400">{t("Total views")}</div>
-                <div className="text-3xl font-Roboto font-bold py-1 flex items-center gap-2 dark:text-white">1428
+                <div className="text-3xl font-Roboto font-bold py-1 flex items-center gap-2 dark:text-white">{thisMonthViews}
                     <span className="text-sm text-green-500 bg-green-200 py-0.5 px-1 rounded-xl flex items-center">
                         <div className="w-5 h-5">
                             <ArrowIcon />
                         </div>
-                        12,55%
+                        {progress}%
                     </span>
                 </div>
                 <div className="font-Poppins text-sm text-neutral-500">{t("Compared to last month")}</div>
