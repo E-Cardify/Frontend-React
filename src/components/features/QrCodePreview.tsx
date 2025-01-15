ce6dc41161f4db31e7b233ea4d1af3062d9e9a0b
zaksiu12s <zaksiu279@gmail.com>
2025-01-15 18:04:07 +0100

    1.9.0 change the appereance of QrCodePreview in dashboard

--- src/components/features/QrCodePreview.tsx
+++ src/components/features/QrCodePreview.tsx
@@ -14,16 +14,22 @@ export default function QrCodePreview(props: {
 
     return (
         <div className="fixed inset-0 z-30">
-            <div className="w-full h-full relative">
-                <div className="absolute inset-0 cursor-pointer z-10 backdrop-blur-md bg-black/80" onClick={handleHideQrCodePreview} />
-                <div className="flex items-center justify-center h-full flex-col gap-5">
-                    <div className="flex items-center gap-1">
+            <div className="w-full h-full relative flex justify-center items-center">
+                {/* backdrop and darken */}
+                <div className="absolute inset-0 cursor-pointer z-10 backdrop-blur-sm bg-black/80" onClick={handleHideQrCodePreview} />
+
+                {/* qr code in the middle */}
+                <div className="from-green-500 to-blue-500 bg-gradient-to-br z-20 rounded-lg border-2 border-black shadow shadow-black">
+                    <div className="flex items-center gap-1 w-full p-5 mb-8">
                         <div className="w-10 h-10 text-red-500 z-50 relative cursor-pointer" onClick={handleHideQrCodePreview}>
                             <XIcon />
                         </div>
-                        <p className="text-3xl font-Poppins font-bold text-white z-50 relative">{t("Your Qr Code")}</p>
+                        <p className="flex-1 text-3xl text-center font-Montserrat font-bold text-white z-50 relative">{t("Your Qr Code")}</p>
+                        <div className="w-10 h-10" />
                     </div>
-                    <QRCode value={props.value} className="w-full h-full max-w-80 max-h-80 relative z-50" />
+                    <div className="w-full h-full relative z-50 px-10 pb-10">
+                        <QRCode value={props.value} className="mx-auto max-w-80 max-h-80 border-4 border-black rounded-lg shadow shadow-neutral-700 box-content" />
+                    </div>
                 </div>
             </div>
         </div>
