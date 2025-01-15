572f1473a94fdbdff8233787c28093daee8009c4
zaksiu12s <zaksiu279@gmail.com>
2025-01-15 18:24:41 +0100

    1.11.0 add Notifications button funcionality

--- src/components/layout/Dashboard/DashboardNavbar.tsx
+++ src/components/layout/Dashboard/DashboardNavbar.tsx
@@ -2,9 +2,23 @@ import { useTranslation } from "react-i18next";
 import { BellIcon, InformationIcon, UserIcon } from "@icons";
 import { DashboardInfoButton } from "./DashboardInfoButton";
 import { DashboardNavbarTabs } from "./DashboardNavbarTabs";
+import useDashboardViewContext from "@hooks/useDashboardViewContext";
+import useViewContext from "@hooks/useViewContext";
 
 export function DashboardNavbar() {
     const { t } = useTranslation();
+    const { currentView, setCurrentView } = useViewContext();
+    const { currentView: currentDashboardView, setCurrentView: setCurrentDashboardView } = useDashboardViewContext();
+
+    const showNotifications = () => {
+        if (currentView !== "Dashboard") {
+            setCurrentView("Dashboard");
+        }
+
+        if (currentDashboardView !== "Notifications") {
+            setCurrentDashboardView("Notifications");
+        }
+    }
 
     return (
         <div className="border-b-2 dark:border-neutral-400">
@@ -19,7 +33,7 @@ export function DashboardNavbar() {
                         <InformationIcon />
                     </DashboardInfoButton>
 
-                    <DashboardInfoButton title={t("Notifications")}>
+                    <DashboardInfoButton onClick={showNotifications} title={t("Notifications")}>
                         <BellIcon />
                     </DashboardInfoButton>
 
