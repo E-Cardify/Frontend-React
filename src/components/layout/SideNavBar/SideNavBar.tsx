15265595b882a6605a3f5801e358779f5ddf3d9e
zaksiu12s <zaksiu279@gmail.com>
2025-01-17 14:43:33 +0100

    1.13.7 fix sidenavbar on mobile devices

--- src/components/layout/SideNavBar/SideNavBar.tsx
+++ src/components/layout/SideNavBar/SideNavBar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CollapseSideNavBarContext } from "@hooks/CollapseSideNavBarContext";
 import { SideNavBarItem } from "./SideNavBarItem";
 import { SideNavBarLogo } from "./SideNavBarLogo";
@@ -10,19 +10,34 @@ import { useTranslation } from "react-i18next";
 export function SideNavBar() {
     const { t } = useTranslation();
     const [isCollapsed, setIsCollapsed] = useState(false);
+    const [isMobile, setIsMobile] = useState((window.innerWidth < 640));
 
     const handleCollapseSideNavBar = () => {
         setIsCollapsed(!isCollapsed);
     }
 
+    useEffect(() => {
+        if (isMobile) {
+            setIsCollapsed(true);
+        }
+    }, [isMobile])
+
+    useEffect(() => {
+        const handleResize = () => setIsMobile(window.innerWidth < 640);
+
+        window.addEventListener("resize", handleResize);
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, [])
+
     return (
         <CollapseSideNavBarContext.Provider value={{ isCollapsed, setIsCollapsed }}>
             <div className="flex  flex-col sticky bg-white dark:bg-neutral-900 w-[240px] h-[95vh] rounded-lg top-[2.5vh] border-2 max-w-max px-2 py-2 left-5 t-5 dark:border-black">
-                <div className="flex justify-between items-center">
+                <div className={`flex items-center ${isMobile ? "justify-center mt-3" : "justify-between"}`}>
                     <SideNavBarLogo />
-                    <div title={!isCollapsed ? t("Hide sidebar") : t("Show sidebar")} className="w-6 h-6 cursor-pointer text-neutral-500 hover:text-neutral-700 dark:hover:text-neutral-400" onClick={handleCollapseSideNavBar}>
+                    {!isMobile && <div title={!isCollapsed ? t("Hide sidebar") : t("Show sidebar")} className="w-6 h-6 cursor-pointer text-neutral-500 hover:text-neutral-700 dark:hover:text-neutral-400" onClick={handleCollapseSideNavBar}>
                         <CollapseIcon />
-                    </div>
+                    </div>}
                 </div>
 
                 <div className="gap-2 pt-6 flex flex-col overflow-x-auto items-center">
