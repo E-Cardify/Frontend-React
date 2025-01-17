0c3d2222862adb8e9d79e8452af3775cab945864
zaksiu12s <zaksiu279@gmail.com>
2025-01-17 14:32:02 +0100

    1.13.4 add some hover effects to buttons

--- src/components/layout/Dashboard/DashboardNavItem.tsx
+++ src/components/layout/Dashboard/DashboardNavItem.tsx
@@ -12,7 +12,7 @@ export function DashboardNavItem(props: {
     };
 
     return (
-        <div className={`pb-2 px-3 dark:text-white relative cursor-pointer ${currentView !== props.text && "text-neutral-500 dark:text-neutral-400"}`} onClick={handleViewChange}>
+        <div className={`pb-2 px-3 dark:text-white relative cursor-pointer ${currentView !== props.text ? "text-neutral-500 hover:text-neutral-600 dark:text-neutral-300 hover:dark:text-neutral-200" : "dark:text-white"}`} onClick={handleViewChange}>
             {t(props.text)}
             <div className={`absolute -bottom-0.5 left-0 w-full h-[3px] ${(currentView === props.text) && "bg-green-500"}`} />
         </div>
--- src/components/layout/SideNavBar/SideNavBarUpgradeButton.tsx
+++ src/components/layout/SideNavBar/SideNavBarUpgradeButton.tsx
@@ -18,7 +18,7 @@ export function SideNavBarUpgradeNowButton() {
                 }
                 <div className="p-0.5">
                     <div className="max-w-[200px] p-3 relative bg-white dark:bg-neutral-900 dark:text-white rounded-md text-xs font-Poppins">
-                        <p>
+                        <p className="select-none">
                             {t("Get detailed analytics to help you, upgrade and go pro")}
                         </p>
                         <button type="button" title="Show Premium Options" className="select-none text-sm mt-3 w-max bg-black text-white dark:bg-white dark:text-black px-2 py-1 rounded-lg">
