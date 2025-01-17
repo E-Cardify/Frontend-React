28fb5d99cd8a7d3c1eb8127d24b7535d29f1b6d3
zaksiu12s <zaksiu279@gmail.com>
2025-01-17 14:25:05 +0100

    1.13.4 add some hover effects to buttons

--- src/components/layout/SideNavBar/SideNavBarLogo.tsx
+++ src/components/layout/SideNavBar/SideNavBarLogo.tsx
@@ -6,8 +6,8 @@ export function SideNavBarLogo() {
 
     return (
         <div>
-            <div className="text-2xl font-Poppins font-bold cursor-pointer flex gap-1 items-center dark:text-white">
-                <div className="w-8 h-8 text-green-600">
+            <div className="text-2xl font-Poppins font-bold cursor-pointer flex gap-1 items-center dark:text-white group">
+                <div className="w-8 h-8 text-green-500 group-hover:text-green-600">
                     <SlackIcon />
                 </div>
                 {!isCollapsed && <h1 className="select-none">Cardify</h1>}
