dee864eb5ca218db14fb75a5b657ea2481467ded
zaksiu12s <zaksiu279@gmail.com>
2025-01-22 12:08:03 +0100

    3.3.4 refactor inputs and textareas in Card Creation

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,4 +1,5 @@
 export default function Input(props: {
+  wFull?: boolean;
   onChange: (
     e: React.ChangeEvent<HTMLInputElement>
   ) => void;
@@ -12,7 +13,11 @@ export default function Input(props: {
       id={props.id}
       value={props.value}
       onChange={props.onChange}
-      className="dark:bg-neutral-800 max-w-80 dark:border-black dark:focus:border-neutral-600 focus:border-neutral-500 outline-none border-2 p-1 px-4 rounded-lg font-Poppins"
+      className={`dark:bg-neutral-800 ${
+        props.wFull
+          ? "w-full"
+          : "max-w-80"
+      } dark:border-black dark:focus:border-neutral-600 focus:border-neutral-500 outline-none border-2 p-1 px-4 rounded-lg font-Poppins`}
     />
   );
 }
