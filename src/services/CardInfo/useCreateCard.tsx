455a774c8cc1e081c19c4bd9cd89060bbe02ddc7
zaksiu12s <zaksiu279@gmail.com>
2025-01-26 10:47:56 +0100

    3.8.1 fix backend connection string

--- src/services/CardInfo/useCreateCard.tsx
+++ src/services/CardInfo/useCreateCard.tsx
@@ -1,16 +1,13 @@
 import { CardInfoInterface } from "@interfaces/CardInfoInterface";
 
 export const createCard = async (cardInfo: CardInfoInterface) => {
-  const response = await fetch(
-    "http://localhost:5000/api/v1/card-info/create",
-    {
-      method: "POST",
-      body: JSON.stringify(cardInfo),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  const response = await fetch("http://localhost:5000/api/v1/card-info/", {
+    method: "POST",
+    body: JSON.stringify(cardInfo),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
   console.log(response);
 };
